6a8fd02047736f332705a8ea693ad541a9f0faea
Alan K <akillian@outlook.com>
2024-05-16 14:23:04 -0400

    commit

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 from langchain.prompts import PromptTemplate
-from langchain.llms import CTransformers
+from langchain_community.llms import CTransformers
 
-# Function to get response from LLAMA 2 Model
+# ================================================ #
+#   Function to get response from LLAMA 2 Model
+# ================================================ #
 
 
 def get_llama_responses(input_text, no_words, category):
+    # pylint: disable-next=not-callable
     llm = CTransformers(model='models\llama-2-7b-chat.ggmlv3.q8_0.bin',
                         model_type='llama',
                         config={'max_new_tokens': 256,
@@ -17,31 +20,37 @@ def get_llama_responses(input_text, no_words, category):
     prompt = PromptTemplate(input_variables=["input_text", "no_words", "category"],
                             template=template)
 
-    # Generate the reponse from the LLama 2 Model
-    respone = llm(prompt.format(category=category,
-                  input_text=input_text, no_words=no_words))
-    print(respone)
-    return respone
+    # Generate the response from the LLama 2 Model
+    response = llm(prompt.format(category=category,
+                                 input_text=input_text, no_words=no_words))
+    print(response)
+    return response
+# ============ End get_llama_responses function ======================== #
 
 
+# ============================================================ #
+# ================ Streamlit Application ===================== #
+# ============================================================ #
 st.set_page_config(page_title="Generate content",
                    layout='centered',
                    initial_sidebar_state="collapsed")
 
 st.header("Creative Writer✍️")
 
-input_text = st.text_input("Enter the topic you want to write about")
+input_text_global = st.text_input("Enter the topic you want to write about")
 
 col1, col2 = st.columns([5, 5])
 
 with col1:
-    no_words = st.text_input('No of words')
+    no_words_global = st.text_input('Number of words')
 with col2:
-    category = st.selectbox("category",
-                            ('Essays', 'Poem', 'Joke', 'Blog'),
-                            index=0)
+    category_global = st.selectbox("Category",
+                                   ('Essays', 'Poem', 'Joke', 'Blog'),
+                                   index=0)
 
 submit = st.button("Generate")
 
 if submit:
-    st.write(get_llama_responses(input_text, no_words, category))
+    st.write(get_llama_responses(input_text_global,
+             no_words_global, category_global))
+# ================ End Streamlit Application ===================== #
